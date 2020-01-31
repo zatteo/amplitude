7284979654b7d505cbd3a590565b6e95b1709764
Geoff Dutton <g.dutton@gmail.com>
2020-01-31 11:16:02 -0600

    fixes lint errors

--- tasks/generate-contributors-list.js
+++ tasks/generate-contributors-list.js
@@ -1,20 +1,19 @@
-'use strict'
-
+const path = require('path')
 const fs = require('fs')
 const PKG = require('../package.json')
-const README_LOCATION = __dirname + '/../README.md'
+const README_LOCATION = path.resolve(__dirname, '..', 'README.md')
 
-function cleanReadme(file) {
-  let lines = file.split('\n')
-  let lastLine = lines.indexOf('------>')
-  let readme = lines.slice(0, lastLine + 1).join('\n')
+function cleanReadme (file) {
+  const lines = file.split('\n')
+  const lastLine = lines.indexOf('------>')
+  const readme = lines.slice(0, lastLine + 1).join('\n')
 
   return readme
 }
 
-function getContributorText() {
+function getContributorText () {
   let text = '\n\n## Contributors\n\n'
-  let contributors = Object.keys(PKG.contributors).map(con => PKG.contributors[con])
+  const contributors = Object.keys(PKG.contributors).map(con => PKG.contributors[con])
 
   contributors.forEach((contributor) => {
     if (contributor.url) {
@@ -22,17 +21,16 @@ function getContributorText() {
     } else {
       text += `+ ${contributor.name}\n`
     }
-  });
+  })
 
   return text
 }
 
-let readmeFile = fs.readFileSync(README_LOCATION, { encoding: 'utf8' })
+const readmeFile = fs.readFileSync(README_LOCATION, { encoding: 'utf8' })
 let readme = cleanReadme(readmeFile)
 
-let contributorText = getContributorText()
+const contributorText = getContributorText()
 
 readme += contributorText
 
 fs.writeFileSync(README_LOCATION, readme)
-
--- test/helpers/globals.js
+++ test/helpers/globals.js
@@ -1,13 +1,13 @@
 'use strict'
 
-let sinon = require('sinon')
-let sinonChai = require('sinon-chai')
-let chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const chai = require('chai')
 
 chai.use(sinonChai)
 
 global.expect = chai.expect
 
 beforeEach(function () {
-  this.sandbox = sinon.createSandbox();
-});
+  this.sandbox = sinon.createSandbox()
+})
